02c9e49d5f08073e29914215dfc681e678adaba6
Amiel Rivier <amiel.rivier@webinage.fr>
2021-05-07 20:18:57 +0200

    Mise au propre

--- test chloé.py
+++ test chloé.py
@@ -1,42 +1,33 @@
 import sqlite3
-connexion = sqlite3.connect("C:\\Users\\schwe\\Desktop\\Groupe 21.db")
+connexion = sqlite3.connect("C:\\Users\\Amiel\\Downloads\\Groupe_21.db")
 curseur = connexion.cursor()
-import time
 
-b=0
-a=curseur.execute("""select Userid from User""")
+def delete_user_content(User_id):
+  queries = [
+  "DELETE FROM Conversation WHERE creator_id = :b",
+  "DELETE FROM Participants WHERE User_id = :b",
+  "DELETE FROM User WHERE userid = :b",
+  "DELETE FROM Settings WHERE settings_user_id = :b",
+  "DELETE FROM contact_list WHERE user_id = :b",
+  "DELETE FROM messages WHERE sender_id = :b"
+  ]
+
+  for query in queries:
+    curseur.execute(query,[User_id])
+  connexion.commit()
+  print ("account id " + str(User_id) + " has been banned")
+
+userids=curseur.execute("select Userid from User").fetchall()
+userids = [i[0] for i in userids]
+
+for userid in userids:
+  e=curseur.execute("select count(blocked_user) from blocklist where blocked_user = :b",[userid]).fetchall()[0][0]
+  if e>=5:
+    delete_user_content(userid)
 
 
-connexion.commit()
-c=len(a.fetchall())
-print(c)
-
-d=curseur.execute("""select blocked_user from blocklist""")
-print(d.fetchall)
-
-for i in range(c):
- b += 1
- e=curseur.execute("""select count(blocked_user) from blocklist where blocked_user = :b""",[b])
- if e=5:
-  def find_user(user_firstname):
-   query = "SELECT id FROM Users WHERE User_id = :User"
-   resultats = curseur.execute(query, {"User": User_firstname})
-   User_id = resultats.fetchone()[0]
-   return user_id
-
-  def delete_user_content(User_id):
-   queries = [
-   "DELETE FROM Conversation WHERE creator_id = :b",
-   "DELETE FROM Participants WHERE Userid = :b",
-   "DELETE FROM Users WHERE userid = :b"
-   "DELETE FROM Settings WHERE settings_userid = :b"
-   "DELETE FROM contact_list WHERE user_id = :b"
-   "DELETE FROM messages WHERE sender_id = :b"
-   ]
-   print ("your account has been banned")
-
-  see_table("User")
-
- 
-connexion.commit()
 connexion.close()
+
+
+
+
