bc43fb674b07aed00fed0ae623033775046a4983
Amiel Rivier <amiel.rivier@webinage.fr>
2021-05-07 22:07:34 +0200

    cedric fix

--- code cedric.py
+++ code cedric.py
@@ -6,35 +6,53 @@
 
 import time
 import sqlite3
-connexion = sqlite3.connect("C:\\Users\\cedri\\Downloads\\database projet.db", timeout = 15)
+connexion = sqlite3.connect("C:\\Users\\Amiel\\Downloads\\Groupe_21.db", timeout = 15)
 curseur = connexion.cursor()
 seconds = time.time()
 
 
+def list_user():
+    return [i[0] for i in curseur.execute("SELECT userid FROM User").fetchall()]
 
-if seconds > 10:
-curseur.execute("""SELECT * FROM Messages""")
-nb_message_utilisateur = curseur.fetchall()
-len(nb_message_utilisateur)
-connexion.commit()
+def get_username(userid):
+    first_and_last_name= curseur.execute("select user_firstname , user_lastname from user where userid = :UserId", [userid]).fetchone()
+    return first_and_last_name[0] + " " + first_and_last_name[1]
 
+def choix_banniere(userid):
+    if curseur.execute("select count(message_id) from Messages where sender_id = :UserId", [userid]).fetchone()[0] > 8:
+        return "MEGA BANNIERE"
+    else:
+        return "BANNIERE STANDARD"
 
-
-x = len(nb_message_utilisateur)
-y = x
-
-
-
-if y < x:
-a = curseur.execute("""SELECT sender_id FROM Messages WHERE message_id = len(nb_message_utilisateur)""")
-#il faut prendre le senderId du message id qu'on vient de prendre
-curseur.execute("""UPDATE User SET User_MessageQuantity = User_MessageQuantity + 1 WHERE User_id = sender_id""")
+for user in list_user():
+    print(choix_banniere(user) + " pour " + get_username(user))
 
 
 
-seconds = 0
+
+# if seconds > 10:
+#     curseur.execute("SELECT * FROM Messages")
+#     nb_message_utilisateur = curseur.fetchall()
+#     len(nb_message_utilisateur)
+#     connexion.commit()
 
 
 
-#on pourrait comparer les chiffres des messages avant de lancer le script des if seconds > 10 pour le comparer
-# avec celui qu'on obtient quand la requête est lancée
+# x = len(nb_message_utilisateur)
+# y = x
+
+
+
+# if y < x:
+#     a = curseur.execute("SELECT sender_id FROM Messages WHERE message_id = len(nb_message_utilisateur)")
+#     #il faut prendre le senderId du message id qu'on vient de prendre
+#     curseur.execute("UPDATE User SET User_MessageQuantity = User_MessageQuantity + 1 WHERE User_id = sender_id")
+
+
+
+# seconds = 0
+
+
+
+# #on pourrait comparer les chiffres des messages avant de lancer le script des if seconds > 10 pour le comparer
+# # avec celui qu'on obtient quand la requête est lancée
