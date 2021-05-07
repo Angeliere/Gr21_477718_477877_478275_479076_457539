a15676df2a951a46eeeb748426458ae484791b22
Angeliere <Alexandre.schweitzer@ulb.ac>
2021-05-07 20:22:26 +0200

    merdouille alex

--- test chloé.py
+++ test chloé.py
@@ -5,20 +5,7 @@ import time
 
 b=0
 a=curseur.execute("""select Userid from User""")
-
-
-connexion.commit()
-c=len(a.fetchall())
-print(c)
-
-d=curseur.execute("""select blocked_user from blocklist""")
-print(d.fetchall)
-
-for i in range(c):
- b += 1
- e=curseur.execute("""select count(blocked_user) from blocklist where blocked_user = :b""",[b])
- if e=5:
-  def find_user(user_firstname):
+def find_user(user_firstname):
    query = "SELECT id FROM Users WHERE User_id = :User"
    resultats = curseur.execute(query, {"User": User_firstname})
    User_id = resultats.fetchone()[0]
@@ -32,11 +19,32 @@ for i in range(c):
    "DELETE FROM Settings WHERE settings_userid = :b"
    "DELETE FROM contact_list WHERE user_id = :b"
    "DELETE FROM messages WHERE sender_id = :b"
-   ]
+   "DELETE FROM messages WHERE sender_id = :b"
    print ("your account has been banned")
+   ]
+   
+
+connexion.commit()
+c=len(a.fetchall())
+print(c)
+
+d=curseur.execute("""select blocked_user from blocklist""")
+print(d.fetchall)
+
+for i in range(c):
+ b += 1
+ e=curseur.execute("select count(blocked_user) from blocklist where blocked_user = :b",[b])
+ if e==5:
+  
 
   see_table("User")
-
  
-connexion.commit()
+
 connexion.close()
+
+
+
+
+
+
+  
